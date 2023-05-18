4c96125043d65ce567cd0728880400d49f08c7f8
TheCSharpAcademy <thecsharpacademy@gmail.com>
2023-05-19 07:32:59 +1000

    Add project files.

---
+++ CoffeeShop.PointOfSale.EntityFramework/ProductController.cs
@@ -0,0 +1,29 @@
+﻿namespace CoffeeShop.PointOfSale.EntityFramework;
+
+internal class ProductController
+{
+    internal static void AddProduct()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void DeleteProduct()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void GetProductById()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void GetProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void UpdateProduct()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ CoffeeShop.PointOfSale.EntityFramework/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CoffeeShop.PointOfSale.EntityFramework;
+using Spectre.Console;
+
+var isAppRunning = true;
+while (isAppRunning)
+{
+    var option = AnsiConsole.Prompt(
+    new SelectionPrompt<MenuOptions>()
+    .Title("What would you like to do?")
+    .AddChoices(
+        MenuOptions.AddProduct,
+        MenuOptions.DeleteProduct,
+        MenuOptions.UpdateProduct,
+        MenuOptions.ViewAllProducts,
+        MenuOptions.ViewProduct));
+
+    switch (option)
+    {
+        case MenuOptions.AddProduct:
+            ProductController.AddProduct();
+            break;
+        case MenuOptions.DeleteProduct:
+            ProductController.DeleteProduct();
+            break;
+        case MenuOptions.UpdateProduct:
+            ProductController.UpdateProduct();
+            break;
+        case MenuOptions.ViewProduct:
+            ProductController.GetProductById();
+            break;
+        case MenuOptions.ViewAllProducts:
+            ProductController.GetProducts();
+            break;
+    }
+}
+
+enum MenuOptions
+{
+    AddProduct,
+    DeleteProduct,
+    UpdateProduct, 
+    ViewProduct,
+    ViewAllProducts,
+    Quit
+}
