415f85aef9c978588a71124a7a15948ce539785b
TheCSharpAcademy <thecsharpacademy@gmail.com>
2023-05-19 09:00:07 +1000

    Creating Database

---
+++ CoffeeShop.PointOfSale.EntityFramework/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace CoffeeShop.PointOfSale.EntityFramework;
+
+internal class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ CoffeeShop.PointOfSale.EntityFramework/ProductsContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.PointOfSale.EntityFramework;
+
+internal class ProductsContext: DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite($"Data Source = products.db");
+}
